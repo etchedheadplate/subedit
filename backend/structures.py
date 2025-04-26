b0e3556a0e8e2001babf54909aecdf464e6819f1
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-04-26 23:18:11 +0300

    Moved API request structures

--- backend/structures.py
+++ backend/structures.py
@@ -1,6 +1,7 @@
-from typing import TypedDict, Dict
+from typing import TypedDict, Dict, Optional, List
+from pydantic import BaseModel
 
-# Types of parsed subtitles data
+# Structures of subtitles data
 class SubtitleMetadata(TypedDict):
     encoding: str
     confidence: float
@@ -18,7 +19,7 @@ class SubtitleData(TypedDict):
 
 SubtitlesDataDict = Dict[str, SubtitleData]
 
-# Structure of external JSON data
+# Structures of external JSON data
 class ModelInfo(TypedDict):
     name: str
     tokens: float
@@ -26,3 +27,41 @@ class ModelInfo(TypedDict):
 class TranslateData(TypedDict):
     codes: Dict[str, str]
     models: Dict[str, ModelInfo]
+
+# Structures of API requests
+class ShowRequest(BaseModel):
+    session_id: str
+    filename: str
+
+class ShiftRequest(BaseModel):
+    session_id: str
+    source_filename: str
+    delay: int
+    items: Optional[List[int]] = None
+
+class AlignRequest(BaseModel):
+    session_id: str
+    source_filename: str
+    example_filename: Optional[str] = None
+    source_slice: Optional[List[int]] = None
+    example_slice: Optional[List[int]] = None
+
+class CleanRequest(BaseModel):
+    session_id: str
+    source_filename: str
+    bold: bool = False
+    italic: bool = False
+    underline: bool = False
+    strikethrough: bool = False
+    color: bool = False
+    font: bool = False
+
+class TranslateRequest(BaseModel):
+    session_id: str
+    source_filename: str
+    target_language: str
+    original_language: str
+    model_name: str
+    model_throttle: float
+    request_timeout: int = 10
+    response_timeout: int = 45
