b578f1c0d33bcb3a63bf6f239a2236c19b5bd3d0
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-04-28 12:09:43 +0300

    Created structures for statistics

--- backend/structures.py
+++ backend/structures.py
@@ -28,6 +28,22 @@ class TranslateData(TypedDict):
     codes: Dict[str, str]
     models: Dict[str, ModelInfo]
 
+class FilesProcessed(TypedDict):
+    shift: int
+    align: int
+    clean: int
+    translate: int
+
+class TranslationTime(TypedDict):
+    total_count_of_responses: int
+    total_responses_duration: float
+    average_response_duration: float
+
+class TranslationData(TypedDict):
+    last_update: int
+    files_processed: FilesProcessed
+    translation_time: TranslationTime
+
 # Structures of API requests
 class ShowRequest(BaseModel):
     session_id: str
