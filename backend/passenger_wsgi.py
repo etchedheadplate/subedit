bf068149576f28e050f1fe95cd02b0e39605a024
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-05-04 18:44:33 +0300

    Updated passenger_wsgi.py

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,1 +1,7 @@
-from main import app as application
+import sys
+import os
+
+# Add the current directory to sys.path
+sys.path.insert(0, os.path.dirname(__file__))
+
+from main import app as application  # FastAPI or ASGI application
