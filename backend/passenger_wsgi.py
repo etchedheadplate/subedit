fafdbd89c6557156bc5a9b6671d69aa97ad0ff57
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-05-06 16:14:36 +0300

    Modified .gitignore

--- backend/passenger_wsgi.py
+++
@@ -1,4 +0,0 @@
-# Dummy file to run uvicorn server
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b"Uvicorn is running. This endpoint is unused."]
