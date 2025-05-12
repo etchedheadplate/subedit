9d5f1d75a7b074477d7af2a77b18bb34a5fd487f
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-05-12 23:35:19 +0300

    Created `TranslatorProtocol` engine class

--- backend/structures.py
+++ backend/structures.py
@@ -1,4 +1,4 @@
-from typing import TypedDict, Dict, Optional, List
+from typing import TypedDict, Dict, Optional, List, Protocol
 from pydantic import BaseModel
 
 # Structures of subtitles data
@@ -19,30 +19,8 @@ class SubtitleData(TypedDict):
 
 SubtitlesDataDict = Dict[str, SubtitleData]
 
-# Structures of external JSON data
-class ModelInfo(TypedDict):
-    name: str
-    tokens: float
-
-class TranslateData(TypedDict):
-    codes: Dict[str, str]
-    models: Dict[str, ModelInfo]
-
-class FilesProcessed(TypedDict):
-    shift: int
-    align: int
-    clean: int
-    translate: int
-
-class TranslationTime(TypedDict):
-    total_count_of_responses: int
-    total_responses_duration: float
-    average_response_duration: float
-
-class TranslationData(TypedDict):
-    last_update: int
-    files_processed: FilesProcessed
-    translation_time: TranslationTime
+class TranslatorProtocol(Protocol):
+    def translate(self, text: str) -> str: ...
 
 # Structures of API requests
 class StatusRequest(BaseModel):
@@ -87,3 +65,33 @@ class TranslateRequest(BaseModel):
     model_throttle: float
     request_timeout: int = 10
     response_timeout: int = 45
+
+# duck.json
+class ModelInfo(TypedDict):
+    name: str
+    tokens: float
+
+class DuckData(TypedDict):
+    codes: Dict[str, str]
+    models: Dict[str, ModelInfo]
+
+# statistics.json
+class FilesProcessed(TypedDict):
+    shift: int
+    align: int
+    clean: int
+    translate: int
+
+class DuckStats(TypedDict):
+    total_count_of_responses: int
+    total_responses_duration: float
+    average_response_duration: float
+
+class StatisticsData(TypedDict):
+    last_update: int
+    files_processed: FilesProcessed
+    duck_statistics: DuckStats
+
+# engines.json
+class LanguagesData(TypedDict):
+    codes: Dict[str, str]
