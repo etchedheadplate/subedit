bd2a16202b37bd034bbcddda0bd2086855b9845d
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2025-05-19 00:31:05 +0300

    Added PWA functionality

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'SubEdit',
+                short_name: 'SubEdit',
+                description: 'Frens editing subtitles',
+                theme_color: '#0D0D0D',
+                background_color: '#0D0D0D',
+                display: 'standalone',
+                orientation: 'portrait',
+                start_url: '/',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
+        }),
+    ],
+});
